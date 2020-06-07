3cc8c482a96b5bdb6841e78d77a61a9129699725
Ben Parish <parishben13@gmail.com>
2020-06-07 15:27:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,58 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader  :side1length, :side2length,:side3length
+ def initialize(side1length,side2length,side3length)
+   @side1length=side1length
+   @side2length=side2length
+   @side3length=side3length
+ end
+ def kind
+   no_greater_than_zero = @side1length.to_f || @side2length.to_f || @side3length.to_f <= 0.00
+   triangle_impossible = ((@side1length.to_f + @side2length.to_f) < @side3length.to_f) || ((@side2length.to_f + @side3length.to_f) < @side1length.to_f) || ((@side1length.to_f + @side3length.to_f) < @side2length.to_f)
+   
+     if
+      @side1length == @side2length && @side2length == @side3length
+       :equilateral
+      
+ elsif @side1length == @side2length  || @side1length == @side3length  || @side2length == @side3length
+    :isosceles 
+  
+   
+elsif @side1length != @side2length && @side2length != @side3length
+
+ :scalene 
+ 
+  end
+  if no_greater_than_zero || triangle_impossible
+     raise TriangleError
+      
+   end
+ end
+
+ class TriangleError < StandardError
+  
+ end
+end
+def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
