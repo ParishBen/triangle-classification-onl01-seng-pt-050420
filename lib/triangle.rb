d4e34224f4fcd7ae5bbbab20505a77d5d0ce5c0f
Ben Parish <parishben13@gmail.com>
2020-06-07 15:48:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
    @side3length=side3length
  end
  def kind
-   no_greater_than_zero = @side1length.to_f || @side2length.to_f || @side3length.to_f <= 0.00
+   no_greater_than_zero = @side1length.to_f <=0.0 || @side2length.to_f  <= 0.0 || @side3length.to_f <= 0.00
    triangle_impossible = ((@side1length.to_f + @side2length.to_f) < @side3length.to_f) || ((@side2length.to_f + @side3length.to_f) < @side1length.to_f) || ((@side1length.to_f + @side3length.to_f) < @side2length.to_f)
    
      if
@@ -33,26 +33,25 @@ elsif @side1length != @side2length && @side2length != @side3length
   
  end
 end
-def kind
-    validate_triangle
-    if a == b && b == c
-      :equilateral
-    elsif a == b || b == c || a == c
-      :isosceles
-    else
-      :scalene
-    end
-  end
+#def kind
+ #  if a == b && b == c
+  #    :equilateral
+   # elsif a == b || b == c || a == c
+    #  :isosceles
+    #else
+     # :scalene
+    #end
+  #end
 
-  def validate_triangle
-    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-    [a, b, c].each do |side|
-      real_triangle << false if side <= 0 
-    raise TriangleError if real_triangle.include?(false)
-    end
-  end
-
-  class TriangleError < StandardError
-  end
-
-end
+  #def validate_triangle
+   # real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    #[a, b, c].each do |side|
+     # real_triangle << false if side <= 0 
+    #raise TriangleError if real_triangle#.include?(false)
+    #end
+#  end
+#
+ # class TriangleError < StandardError
+  #end
+#
+#end
